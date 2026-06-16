a60ea266fdd59c325ffef3d05927c3c520ce6ded
wajdan <wejdan95.almamari@gmail.com>
2026-06-16 07:55:44 +0400

    Add Room class

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -2,6 +2,16 @@
 {
     internal class Program
     {
+        // Room class stores hotel room information
+        public class Room
+        {
+            public int RoomNumber { get; set; }
+            public string RoomType { get; set; }
+            public double PricePerNight { get; set; }
+            public bool IsAvailable { get; set; }
+
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
